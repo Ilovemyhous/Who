d8742bf93ebfee18c5974d61b0de1b3f04769a07
Ilovemyhouse <50217071+Ilovemyhous@users.noreply.github.com>
2022-02-04 09:43:58 +0100

    Finished the settings page.

--- Settings/JS.js
+++ Settings/JS.js
@@ -6,13 +6,70 @@ if (promise !== undefined) {
     // Autoplay allowed!
     console.log("Autoplay enabled");
     //document.getElementById("autoplay_success").innerHTML = "Autoplay is enabled, the video above should play."
-    document.write("Autoplay is enabled, the video above should play.")
+    document.getElementById("autoplay_success").innerHTML = "Autoplay is enabled, the video above should play."
+    document.getElementById("autoplay_success").style.color = "green"
   }).catch(error => {
     // Autoplay not allowed!
-    document.getElementById("autoplay_fail").innerHTML = "Autoplay has to be enabled for the game."
+    console.log("Autoplay disabled");
+    document.getElementById("autoplay_fail").innerHTML = "Autoplay has to be enabled for the game. Enable it, and reload the page."
+    document.getElementById("autoplay_fail").style.color = "red"
     //alert("Autoplay has to be enabled for the game.")
   });
 }
 
-document.cookie = "music=true; expires=11 Feb 2022 12:00:00 UTC";
-document.cookie = "sounds=true; expires=11 Feb 2022 12:00:00 UTC"; 
+//document.cookie = "music=true; expires=11 Feb 2022 12:00:00 UTC; SameSite=None; Secure";
+//document.cookie = "sounds=true; expires=11 Feb 2022 12:00:00 UTC; SameSite=None; Secure";
+
+//Source: https://stackoverflow.com/questions/11801042/getting-a-checkbox-to-write-a-javascript-cookie
+// Cookie for the music
+function setCookie(c_name,value,expiredays) {
+  var exdate=new Date()
+  exdate.setDate(exdate.getDate()+expiredays)
+  document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate)
+}
+
+function getCookie(c_name) {
+  if (document.cookie.length>0) {
+      c_start=document.cookie.indexOf(c_name + "=")
+      if (c_start!=-1) { 
+          c_start=c_start + c_name.length+1 
+          c_end=document.cookie.indexOf(";",c_start)
+          if (c_end==-1) c_end=document.cookie.length
+              return unescape(document.cookie.substring(c_start,c_end))
+      } 
+  }
+  return null
+}
+onload=function(){
+document.getElementById('music').checked = getCookie('music')==1? true : false;
+}
+function set_check_music(){
+setCookie('music', document.getElementById('music').checked? 1 : 0, 100);
+}
+
+//Source: https://stackoverflow.com/questions/11801042/getting-a-checkbox-to-write-a-javascript-cookie
+// Cookie for the sounds
+function setCookie(c_name,value,expiredays) {
+  var exdate=new Date()
+  exdate.setDate(exdate.getDate()+expiredays)
+  document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate)
+}
+
+function getCookie(c_name) {
+  if (document.cookie.length>0) {
+      c_start=document.cookie.indexOf(c_name + "=")
+      if (c_start!=-1) { 
+          c_start=c_start + c_name.length+1 
+          c_end=document.cookie.indexOf(";",c_start)
+          if (c_end==-1) c_end=document.cookie.length
+              return unescape(document.cookie.substring(c_start,c_end))
+      } 
+  }
+  return null
+}
+onload=function(){
+document.getElementById('sounds').checked = getCookie('sounds')==1? true : false;
+}
+function set_check_sounds(){
+setCookie('sounds', document.getElementById('sounds').checked? 1 : 0, 100);
+}
