0a1750a5bf70a4ba36b7c621e9406235b0273b08
Ilovemyhous <50217071+Ilovemyhous@users.noreply.github.com>
2022-03-12 15:18:00 +0100

    Changed the Background for the hub.

--- Game/Error/JS.js
+++ Game/Error/JS.js
@@ -2,14 +2,14 @@
 console.log(localStorage);
 console.log(localStorage.sound);
 
-if (localStorage.sound === "on"){
+if (localStorage.sound === "off"){
+    console.log("else audio: "+localStorage.sound);
+    console.log("No audio");
+}
+
+else {
     console.log("if audio: "+localStorage.sound);
     var audio = new Audio("../../Media/Sound/Black hole.wav");
     console.log("Debug audio")
     audio.play();
-}
-
-else {
-    console.log("else audio: "+localStorage.sound);
-    console.log("No audio");
 }
