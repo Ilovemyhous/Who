c32ec4ac7109257a8b81f9b36e526d5eb2c42e78
Amiralgaby <gaby.theuws@gmail.com>
2022-03-13 11:57:20 +0100

    creation de la base d'une factory de typer element

--- Game/Introduction/JS.js
+++ Game/Introduction/JS.js
@@ -2,10 +2,40 @@
 console.log(localStorage);
 console.log(localStorage.sound);
 
+function create_element_typer(text_to_print) {
+    const paragraph = document.createElement("p");
+    paragraph.classList.add("typer");
+    paragraph.setAttribute('data-colors', 'gray');
+    paragraph.setAttribute('data-words',text_to_print);
+    paragraph.setAttribute('data-delay',"100");
+    paragraph.setAttribute('id','creationEl');
+    paragraph.setAttribute('data-loop',"1");
+    return paragraph;
+}
+
+function proceed_to_typer_element(text_to_print) {
+    const my_element = create_element_typer(text_to_print);
+    document.querySelector('h2').appendChild(my_element);
+    new Typer(my_element);
+}
+
+function code(){
+    setTimeout(function(){
+        let name;
+        // System that prevents empty names
+        do{
+            name = prompt("Who are you?");
+        }while(name == "" || name == null || name == undefined);
+        
+        console.log("Name: "+name);
+        proceed_to_typer_element("Hello..." + name + " ...");
+        // document.getElementById("asking").style.visibility = "visible"
+    }, 500);
+}
+
 if (localStorage.sound === "off"){
     console.log("if audio: "+localStorage.sound);
     console.log("No audio");
-    code()
 }
 
 else {
@@ -13,30 +43,12 @@ else {
     var audio = new Audio("../../Media/Sound/Tuesday T.wav");
     console.log("Debug audio")
     audio.play();
-    var audio = new Audio("../../Media/Sound/Computer typing.mp3");
+    audio = new Audio("../../Media/Sound/Computer typing.mp3");
     audio.play();
     setTimeout(function(){
-        var audio = new Audio("../../Media/Sound/Computer typing 2.wav");
+        audio = new Audio("../../Media/Sound/Computer typing 2.wav");
         audio.play();
     }, 7500)
-    code()
 }
 
-function code(){
-    setTimeout(function(){
-        name = prompt("Who are you?")
-        // System that prevents empty names
-        i = 0
-        while (i == 0) {
-            if (name == "" || name == "null"){
-            name = prompt("Who are you?")
-            //console.log("Debug name");
-        }
-        else {
-            i = i + 1
-        }
-        console.log("Name: "+name);
-        //document.getElementById("asking").style.visibility = "visible"
-        }
-    }, 11500)
-}
+code();
