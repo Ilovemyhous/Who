867433f00d143166679ef5a469e96d724c018afe
Ilovemyhous <50217071+Ilovemyhous@users.noreply.github.com>
2022-03-13 16:02:14 +0100

    Updated introduction. Huge thanks to @Amiralgaby!

--- Game/Introduction/JS.js
+++ Game/Introduction/JS.js
@@ -2,41 +2,84 @@
 console.log(localStorage);
 console.log(localStorage.sound);
 
-if (localStorage.sound === "off"){
-    console.log("if audio: "+localStorage.sound);
-    console.log("No audio");
-    code()
-}
-
-else {
-    console.log("else audio: "+localStorage.sound);
-    var audio = new Audio("../../Media/Sound/Tuesday T.wav");
-    console.log("Debug audio")
-    audio.play();
-    var audio = new Audio("../../Media/Sound/Computer typing.mp3");
-    audio.play();
-    setTimeout(function(){
-        var audio = new Audio("../../Media/Sound/Computer typing 2.wav");
+function audio() {
+    if (localStorage.sound === "off"){
+        console.log("if audio: "+localStorage.sound);
+        console.log("No audio");
+    }
+    else {
+        console.log("else audio: "+localStorage.sound);
+        var audio = new Audio("../../Media/Sound/Tuesday T.wav");
+        console.log("Debug audio")
         audio.play();
-    }, 7500)
-    code()
+        audio = new Audio("../../Media/Sound/Computer typing.mp3");
+        audio.play();
+    }
 }
 
-function code(){
-    setTimeout(function(){
-        name = prompt("Who are you?")
-        // System that prevents empty names
-        i = 0
-        while (i == 0) {
-            if (name == "" || name == "null"){
-            name = prompt("Who are you?")
-            //console.log("Debug name");
-        }
-        else {
-            i = i + 1
-        }
-        console.log("Name: "+name);
-        //document.getElementById("asking").style.visibility = "visible"
-        }
-    }, 11500)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+function create_element_typer(text_to_print) {
+    const paragraph = document.createElement("span");
+    paragraph.classList.add("typer");
+    paragraph.setAttribute('data-colors', 'gray');
+    paragraph.setAttribute('data-words',text_to_print);
+    paragraph.setAttribute('data-delay',"100");
+    paragraph.setAttribute('id','creationEl');
+    paragraph.setAttribute('data-loop',"1");
+    return paragraph;
+}
+
+function proceed_to_typer_element(text_to_print) {
+    const div = document.createElement("div");
+    const my_element = create_element_typer(text_to_print);
+    div.appendChild(my_element);
+    const cursor = document.querySelector('.cursor');
+    div.appendChild(cursor);
+    document.querySelector('h2').appendChild(div);
+    new Typer(my_element);
+}
+
+function audio_typing() {
+    if (localStorage.sound === "off"){
+        console.log("if audio: "+localStorage.sound);
+        console.log("No audio");
+    }
+    else {
+        console.log("else audio: "+localStorage.sound);
+        var audio = new Audio("../../Media/Sound/Computer typing 2.wav");
+        console.log("Debug audio")
+        audio.play();
+    }
+}
+
+async function code(){
+
+    await sleep(5000);
+    proceed_to_typer_element(". . .");
+
+    await sleep(3000)
+    proceed_to_typer_element("Computer: Who... who are you?");
+    audio_typing()
+
+    await sleep(5000);
+    
+    let name;
+
+    // System that prevents empty names
+    do{
+        name = prompt("Who are you ?");
+    }while(name == "" || name == null || name == undefined);
+
+    await sleep(2500);
+    audio_typing()
+    proceed_to_typer_element("Computer: Hello... " + name + "...");
+
+    await sleep(5000);
+    proceed_to_typer_element("Computer: Wha... what happened?")
+    audio_typing()
+}
+
+code();
