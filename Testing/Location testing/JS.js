c3333c98eca5c80c3492954739c2f1ee25c5be62
Ilovemyhouse <50217071+Ilovemyhous@users.noreply.github.com>
2022-10-24 12:21:39 +0000

    Delete Testing directory

--- Testing/Location testing/JS.js
+++
@@ -1,6 +0,0 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-  gps_long = position.coords.latitude
-  gps_lat = position.coords.longitude
-  console.log(gps_long);
-  console.log(gps_lat);
-});
